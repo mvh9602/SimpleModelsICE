cb32cceca11a12aa46fd1c0c4c5b698a9d94f098
Matt Hoffman <mvh9602@g.rit.edu>
2020-03-02 14:42:28 -0500

    added in files from example

---
+++ server/models/Cat.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+let CatModel = {};
+
+const CatSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  bedsOwned: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+CatSchema.statics.findByName = (name, callback) => {
+  return CatModel.findOne({name}, callback);
+};
+
+CatModel = mongoose.model('Cat', CatSchema);
+
+module.exports = {
+  CatModel,
+  CatSchema
+}
+
+
+
+
+
